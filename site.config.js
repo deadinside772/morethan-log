e43707db1d445f1236737f7a142a0bd92c503c52
harshit mahajan <91603517+deadinside772@users.noreply.github.com>
2024-06-19 19:29:58 +0530

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,14 +18,14 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "harshit's Copy & Finance",
+    title: "Harshit's Copy & Finance",
     description: "welcome to my Copy & Finance!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2021, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
